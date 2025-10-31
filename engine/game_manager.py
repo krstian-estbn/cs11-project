8cd31a983c425d9ea8a6320bc61d654eeb57390d
misteryeaster <nicoatienza72@gmail.com>
2025-11-01 02:05:23 +0800

    fix: reset bug

--- engine/game_manager.py
+++ engine/game_manager.py
@@ -5,11 +5,6 @@ from engine.renderer import Renderer
 from world.map import Map
 from player.player_manager import Player
 
-
-input_handler = InputHandler()
-renderer = Renderer()
-maps = Map()
-
 class GameManager:
     def __init__(self):
         self.input_handler = InputHandler()
@@ -17,7 +12,7 @@ class GameManager:
         self.maps = Map()
         self.player = None
         self.map_level = None
-        self.mushrooms = 0
+        self.mushroom_count = 0
         self.row_len = 0
         self.col_len = 0
 
@@ -27,12 +22,32 @@ class GameManager:
         time.sleep(1)
         return True
 
+    def initialize_game(self, map_file="test.txt"):
+        """initialize or reset the game state"""
+        self.map_level = self.maps.map_generator(map_file)
+        (r, c), self.mushroom_count = self.maps.initial_player_pos(self.map_level)
+
+        if (r, c) == (-1, -1):
+            print("Invalid Map: No 'L' in Map!")
+            return False
+        
+        self.player = Player(r, c)
+        self.row_len = len(self.map_level)
+        self.col_len = len(self.map_level[0])
+
+        return True
+
     def game_loop(self):
+
+        #initialize
+        if not self.initialize_game():
+            return False
+
         while self.player.status:
-            renderer.display_map(self.map_level, self.player.points, self.player.under_l, self.player.current_item)
+            self.renderer.display_map(self.map_level, self.player.points, self.player.under_l, self.player.current_item)
             
             try:
-                move_input = input_handler.get_input()
+                move_input = self.input_handler.get_input()
             except EOFError:
                 continue
 
@@ -43,15 +58,15 @@ class GameManager:
                     self.player.pickup_item()
                 if move == 'Q':
                     quit()
-                self.player.movement(self.map_level, move, input_handler.moves, self.row_len, self.col_len)
+                self.player.movement(self.map_level, move, self.input_handler.moves, self.row_len, self.col_len)
 
                 if self.player.points == self.mushroom_count:
-                    renderer.display_map(self.map_level, self.player.points, self.player.under_l, self.player.current_item)
+                    self.renderer.display_map(self.map_level, self.player.points, self.player.under_l, self.player.current_item)
                     print("\n\nYou Won!")
                     return False
                 
                 # checks if win condition is satisfied
                 if not self.player.status:
-                    renderer.display_map(self.map_level, self.player.points, self.player.under_l, self.player.current_item)
+                    self.renderer.display_map(self.map_level, self.player.points, self.player.under_l, self.player.current_item)
                     print("\n\nGame Over!")
                     return False
