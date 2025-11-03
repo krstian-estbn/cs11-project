f85be27e136ff48732668e0585aa3b99d9f607a6
misteryeaster <nicoatienza72@gmail.com>
2025-11-03 15:11:44 +0800

    moved items to item class

--- player/player_manager.py
+++ player/player_manager.py
@@ -1,3 +1,5 @@
+from items.item import Item
+
 class Player:
     """Handles player behavior"""
 
@@ -8,63 +10,15 @@ class Player:
         self.under_l = "."
         self.under_r = {}
         self.status = True
-        self.current_item = ''
+        self.item = None
         
     def pickup_item(self):
         if self.under_l in ('x', '*'):
             self.current_item = self.under_l
+            self.item = Item(self.under_l)
             self.under_l = '.'
         else:
             pass
-            
-    def axe(self, map_level, r, c):
-        map_level[self.r][self.c] = self.under_l
-
-        # removes tree and removes axe
-        self.under_l = '.'
-        self.current_item = ''
-
-        self.r, self.c = r, c
-        map_level[self.r][self.c] = 'L'
-        return
-        
-    def flamethrower(self, map_level, x, y):
-        map_level[self.r][self.c] = self.under_l
-
-        # removes first tree and removes flamethrower
-        self.under_l = '.'
-        self.current_item = ''
-
-        self.r, self.c = x, y
-        map_level[self.r][self.c] = "L"
-
-        # add adjacent trees to player
-        burn = set()
-        for adj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-            neighbor = (adj[0] + self.r, adj[1] + self.c)
-            if 0 <= neighbor[0] < len(map_level) and 0 <= neighbor[1] < len(map_level[0]) and map_level[neighbor[0]][neighbor[1]] == "T":
-                            burn.add(neighbor)
-
-        visited = set()
-        for neigh in burn:
-            adj_trees = [neigh]
-
-            while adj_trees:
-                i, j = adj_trees.pop()
-                if (i, j) in visited:
-                    continue
-                
-                #remove tree
-                visited.add((i, j))
-                map_level[i][j] = "."
-                
-                # add all adjacent trees of the adjacent trees to the list
-                for adj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    neighbor = i + adj[0] , j + adj[1]
-                    if 0 <= neighbor[0] < len(map_level) and 0 <= neighbor[1] < len(map_level[0]) and (neighbor[0], neighbor[1]) not in visited:
-                        if map_level[neighbor[0]][neighbor[1]] == "T":
-                            adj_trees.append((neighbor[0], neighbor[1]))
-        
     
     def movement(self, map_level, move, moves, row_len, col_len):
         action = moves[move]
@@ -82,10 +36,9 @@ class Player:
         target_pos = map_level[new_r][new_c]
         
         if target_pos == "T":
-            if self.current_item == 'x':
-                self.axe(map_level, new_r, new_c)
-            elif self.current_item == '*':
-                self.flamethrower(map_level, new_r, new_c)
+            if self.item != None:
+                self.under_l, self.r, self.c = self.item.use(map_level, self.under_l, self.r, self.c, new_r, new_c)
+                del self.item
             else:
                 return
         # tree function                              
